4ec860c93006d3ac8d9cf0e76a9d68aa595a7595
walegithub2023 <ogundejiwaletemitope@gmail.com>
2023-11-27 14:35:21 +0100

    adding my ReactJS checkpoint project to ReactJS checkpoint repo on github

--- README.md
+++ README.md
@@ -1,70 +1,21 @@
-# Getting Started with Create React App
+# NAME OF REPOSITORY: React_JS_Checkpoint
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Available Scripts
 
-In the project directory, you can run:
+# DESCRIPTION: A repository for ReactJS Checkpoint. This repository houses a project made using ReactJS. The following are steps/instructions used to create the project.
 
-### `npm start`
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+    .Create a react-app using the command `npx create-react-app <project-name>` (make sure to replace <project-name> with your project name).
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+    .After creating the react-app, delete all files in the ‘src’ folder except for reportWebVitals.js and index.js
 
-### `npm test`
+    .Create a new file called App.js containing this project's App component. Make sure to import everything you need to import, including react-bootstrap.
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+    .Check out the react-bootstrap documentation, go to the ‘Get Started’ webpage, and install and import the needed modules as specified in the steps.
 
-### `npm run build`
+    .Inside the App component, create a react fragment (search for react fragments and their uses), and create inside it a div with the className of “App.”
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+    .Inside the div, use react-bootstrap to create a navbar, a heading, and 3 cards (search for navbars, cards, and heading in react-bootstrap).
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+    .When you finish, run your project using the command `npm start.`
+ 
--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+//import React and React bootstrap
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//import container, nav, navbar and navdropdown from react-boostrap
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+//import row and col components from react-boostrap
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+//import button, card  and form components from react boostrap
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //create a React fragment
+    <>                    
+      <div className="App">
+       {/* create navbar */}
+              <Navbar expand="lg" className="bg-primary p-4 text-light mb-2 mb-lg-0" style={{boxShadow: "rgba(0, 0, 0, 0.25) 0px 54px 55px"}}>
+                  <Container>
+                        <Navbar.Brand href="#home" className="h1 text-light" style={{fontSize:"160%", fontWeight:"bold"}}>GomySHOP</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                          <Nav className="me-auto text-light">
+                            <Nav.Link href="#home" className="text-light">Home</Nav.Link>
+                            <Nav.Link href="#produts" className="text-light">Produts</Nav.Link>
+                            <Nav.Link href="#help" className="text-light">Help</Nav.Link>
+                            <Nav.Link href="#aboutUs" className="text-light">About us</Nav.Link>
+                          </Nav>
+                          <Form className="d-flex">
+                          <Form.Control
+                            type="search"
+                            placeholder="Search"
+                            className="me-2"
+                            aria-label="Search"
+                            style={{borderRadius:"0px"}}
+                          />
+                        <Button variant="outline-light" style={{borderRadius:"0px"}}>Search</Button>
+                        </Form>
+                        </Navbar.Collapse>
+                  </Container>
+            </Navbar>
+
+
+
+{/* card section */}
+              <Container  className="mt-5">
+                  <Row>
+                    <Col xs>
+                          <Card className="p-4" style={{ width: '20rem', boxShadow: "rgba(0, 0, 0, 0.25) 0px 54px 55px"}}>
+                          <Card.Img variant="top" src="product1.jpg" />
+                          <Card.Body style={{height:"235px"}}>
+                          <Card.Title>Oraimo FreePods 4 </Card.Title>
+                          <Card.Text>
+                          Oraimo FreePods 4 True Wireless Stereo Earbuds + Free OCW-U65S Charger. - ₦ 30,999
+                          </Card.Text>
+                          <Button variant="primary" style={{borderRadius:"0px"}}>Buy Device</Button>
+                          </Card.Body>
+                          </Card>
+                    </Col>
+
+                    <Col xs>
+                          <Card className="p-4" style={{ width: '20rem', boxShadow: "rgba(0, 0, 0, 0.25) 0px 54px 55px" }}>
+                          <Card.Img variant="top" src="product5.jpg" />
+                          <Card.Body style={{height:"235px"}}>
+                          <Card.Title>Ace Elec WL82 Bluetooth 5.0 Stereo wrist watch</Card.Title>
+                          <Card.Text>
+                          Ace Elec WL82 Bluetooth 5.0 Stereo wrist watch - ₦ 300,999
+                          </Card.Text>
+                          <Button variant="primary" style={{borderRadius:"0px"}}>Buy Device</Button>
+                          </Card.Body>
+                          </Card>
+                    </Col>
+
+                    <Col xs>
+                    <Card className="p-4" style={{ width: '20rem', boxShadow: "rgba(0, 0, 0, 0.25) 0px 54px 55px"}}>
+                          <Card.Img variant="top" src="product3.jpg" />
+                          <Card.Body style={{height:"237px"}}>
+                          <Card.Title>Tecno H3 Wireless Bluetooth Earbuds</Card.Title>
+                          <Card.Text>
+                          Tecno H3 Wireless Bluetooth Earbuds
+                          </Card.Text>
+                          <Button variant="primary" style={{borderRadius:"0px"}}>Buy Device</Button>
+                          </Card.Body>
+                          </Card>
+                    </Col>
+                  </Row>
+            </Container>
+      </div>
+      <div className="bg-primary text-light text-center" style={{height:"70px", marginTop:"70px", paddingTop:"20px"}}>Copyright @ gomySHOP 2023</div>
+    </>
   );
 }
 
